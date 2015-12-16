4246c8174c8c856dec9f073d3c48c90da5dc03a9
Erick Nilsen Pereira de Souza - DATAPREVCE <erick.nilsen@dataprev.gov.br>
2015-12-16 10:42:00 -0300

    Gráfico de rendimento de aluno

--- SAD/WebContent/avaliacao_aluno/js/controller/RendimentoController.js
+++ SAD/WebContent/avaliacao_aluno/js/controller/RendimentoController.js
@@ -1,55 +1,79 @@
-app.controller('RendimentoController', function($scope, $http, $cookies, $rootScope, $sce) {
-
+app.controller('RendimentoController', function($scope, $http, $cookies,
+		$rootScope, $sce) {
 
 	var options = {
-        responsive:true
-    };
+		responsive : true
+	};
 
-    var data = {
-        labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
-        datasets: [
-        	{
-                label: "Meu rendimento",
-                fillColor: "rgba(151,187,205,0.2)",
-                strokeColor: "rgba(151,187,205,1)",
-                pointColor: "rgba(151,187,205,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(151,187,205,1)",
-                data: [28, 48, 40, 19, 86, 27, 90, 200, 87, 20, 50, 20]
-            },
-            {
-                label: "Rendimento da turma",                
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [18, 28, 10, 59, 46, 57, 90, 200, 87, 20, 50, 20]
-            }            
-        ]
-    };
+	var data = {
+		labels : [],
+		datasets : [ {
+			label : "Meu rendimento",
+			fillColor : "rgba(151,187,205,0.2)",
+			strokeColor : "rgba(151,187,205,1)",
+			pointColor : "rgba(151,187,205,1)",
+			pointStrokeColor : "#fff",
+			pointHighlightFill : "#fff",
+			pointHighlightStroke : "rgba(151,187,205,1)",
+			data : []
+		}, {
+			label : "Rendimento médio da turma",
+			fillColor : "rgba(220,220,220,0.2)",
+			strokeColor : "rgba(220,220,220,1)",
+			pointColor : "rgba(220,220,220,1)",
+			pointStrokeColor : "#fff",
+			pointHighlightFill : "#fff",
+			pointHighlightStroke : "rgba(220,220,220,1)",
+			data : []
+		} ]
+	};
 
-	
-    var ctx = document.getElementById("grafico").getContext("2d");
-    var chart = new Chart(ctx);      
-    var lineChart = chart.Line(data, options);  
-
-    document.getElementById("legenda").innerHTML = lineChart.generateLegend();
-
-    
 	$scope.aluno = {};	
 
+	$scope.listarAvaliacoesPorTurmaAluno = function() {
+
+		$http.get(
+				'http://' + $rootScope.ip + ':8080/SAD/rest/alunos/'
+						+ $cookies.get('idAluno')).success(function(dados) {
+			$scope.aluno = dados;
+			$scope.listarAvaliacoesPorTurma();
+		});
+
+	};
+
+	$scope.listarAvaliacoesPorTurma = function() {
+
+		$http.get(
+				'http://' + $rootScope.ip + ':8080/SAD/rest/avaliacoes/t/'
+						+ $scope.aluno.idTurma + '/' + $scope.aluno.id)
+				.success(function(dados) {
+					$scope.listaAvaliacoes = dados;
+					$scope.gerarGraficoRendimento();
+				});
+
+	};
+
 	$scope.gerarGraficoRendimento = function() {
-		$http.get('http://'+$rootScope.ip+':8080/SAD/rest/avaliacoes/'+$scope.avaliacao.id).success(
-			function(dados) {		
-							
-				$scope.aluno.id = $cookies.get('idAluno');							
-			}
-		);	
-	};	
-	
+
+		var rendimentoAluno = [];
+		var avaliacoes = [];
+		for (var i = 0; i < $scope.listaAvaliacoes.length; i++) {		
+			 rendimentoAluno.push(parseFloat($scope.listaAvaliacoes[i].rendimento));
+			 avaliacoes.push($scope.listaAvaliacoes[i].dataCriacao);
+			 
+		}
+
+		data.datasets[0].data = rendimentoAluno;
+		data.labels = avaliacoes;
+		
+		var ctx = document.getElementById("grafico").getContext("2d");
+		var chart = new Chart(ctx);
+		var lineChart = chart.Line(data, options);
+
+		document.getElementById("legenda").innerHTML = lineChart.generateLegend();
+	}
+
+	$scope.listarAvaliacoesPorTurmaAluno();
 
 	
 
